6db991acaa811b043b5893de343dd51211298853
Mijeong Ban <mban1@stevens.edu>
2019-02-04 02:24:13 -0500

    Updates

---
+++ precache-manifest.004496b3df81d27ab5444a6c13a34318.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0fb593c74cb2921817e",
+    "url": "/movie-web-react/static/css/main.cab729a0.chunk.css"
+  },
+  {
+    "revision": "c0fb593c74cb2921817e",
+    "url": "/movie-web-react/static/js/main.c0fb593c.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie-web-react/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "2936225f35288f11303a",
+    "url": "/movie-web-react/static/js/runtime~main.2936225f.js"
+  },
+  {
+    "revision": "7ec91834085e10cb15a6cdee28be4735",
+    "url": "/movie-web-react/index.html"
+  }
+];
