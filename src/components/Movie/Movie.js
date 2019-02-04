56755dbb54ff992000d4db8795b82265dd5d90ed
Mijeong Ban <mban1@stevens.edu>
2019-02-04 00:18:19 -0500

    component lifecycle

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
 
 class Movie extends Component {
-    /* to check correct type of props */
+  /* to check correct type of props */
   static propTypes = {
-    title: PropTypes.string,
-    poster: PropTypes.string
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+  };
+  render() {
+    return (
+      <div>
+        <MoviePoster poster={this.props.poster} />
+        <h1>{this.props.title}</h1>
+      </div>
+    );
   }
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} /> 
-                <h1>{this.props.title}</h1> 
-
-            </div>
-        )
-    }
 }
 
 class MoviePoster extends Component {
-    render() {
-        return (
-            <img src={this.props.poster} />
-        )
+    static propTypes = {
+        poster: PropTypes.string.isRequired
     }
+  render() {
+    return <img src={this.props.poster} />;
+  }
 }
 
 export default Movie;
